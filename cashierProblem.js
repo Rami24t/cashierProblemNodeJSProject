082295da0497705d3035abe8752c06b9c838aee2
Rami <103028944+Rami24t@users.noreply.github.com>
2022-09-07 17:07:32 +0200

    Update cashierProblem.js

--- cashierProblem.js
+++ cashierProblem.js
@@ -48,15 +48,15 @@ function getChange(price = Infinity, paidAmount = 0) {
     price = processAmountInput(price);
     paidAmount = processAmountInput(paidAmount);
     if (price === -1 || paidAmount === -1)
-        return 'Invalid input!';
-    let output = `\n                             ${(new Date()).toString().slice(4, 21)}\n`;
+        return '\x1b[91mInvalid input!\x1b[39m';
+    let output = `\n                             ${(new Date()).toString().slice(4, 21)}\n `;
     let amount = (paidAmount * 100 - price * 100);
     if (price > paidAmount)
-        return `\nPrice is greater than the paid amount!\n (Price: ${price.toFixed(2)} EUR, Paid amount: only ${paidAmount.toFixed(2)} EUR.)\nPlease increase the paid amount by at least: ${(price - paidAmount).toFixed(2)} EUR.\n`;
+        return `\n \x1b[91mPrice is greater than the paid amount!\x1b[39m\n  (\x1b[3mPrice: ${price.toFixed(2)} EUR, Paid only ${paidAmount.toFixed(2)} EUR.)\x1b[23m\n Please increase the paid amount by at least: \x1b[91m\x1b[1m${(price - paidAmount).toFixed(2)} EUR\x1b[22m\x1b[39m.\n `;
     else
-        output += 'Price: €' + price.toFixed(2) + '\nReceived: €' + paidAmount.toFixed(2) + '\nChange: €' + (amount / 100).toFixed(2) + '\n';
+        output += '\x1b[91mPrice: €' + price.toFixed(2) + '\x1b[39m\n \x1b[32mReceived: €' + paidAmount.toFixed(2) + '\n \x1b[97mChange: \x1b[1m€' + (amount / 100).toFixed(2) + '\x1b[22m\n ';
     if (price !== paidAmount)
-        output += (amount > 5 ? '' : 'Coins ') + 'To Return:\n';
+        output += (amount > 5 ? '' : 'Coins ') + '\x1b[3mTo Return:\x1b[23m\n ';
     const changesEuro = [
         50000, 20000, 10000, 5000, 2000, 1000, 500,
         200, 100,
@@ -66,26 +66,30 @@ function getChange(price = Infinity, paidAmount = 0) {
         let number = Math.floor(amount / change);
         amount -= number * change;
         if (number > 0)
-            output += ' - ' + number + ' x €' + (idx > 8 ? (changesEuro[idx] / 100).toFixed(2) : changesEuro[idx] / 100) + (idx > 6 ? ' coin' : ' note') + (number > 1 ? 's' : '') + '\n';
+            output += ' - ' + '\x1b[1m' + number + '\x1b[22m' + ' x \x1b[1m\x1b[3m€' + (idx > 8 ? (changesEuro[idx] / 100).toFixed(2) : changesEuro[idx] / 100) + '\x1b[22m' + (idx > 6 ? ' coin' : ' note') + (number > 1 ? 's' : '') + '\x1b[23m\n ';
     });
     return output;
 }
 function main() {
+    console.log("\x1b[33m[Rami's Cashier Helper] 1.0.18\x1b[89m");
     let paidAmount = 0;
     let price;
     while (paidAmount !== -1 && paidAmount !== 'Q' && paidAmount !== 'q') {
-        price = readline.question('Enter Total Price: (or \'Q\' to quit) €');
+        console.log("\x1b[33m\n [Rami's Cashier Helper] to restart, press 'Enter' key without entering any value\n [Rami's Cashier Helper] To quit at any time, enter 'q'\x1b[89m\n \x1b[0m")
+        price = readline.question(' Enter Total Price: (or \'Q\' to quit) €');
         if (price === 'Q' || price === 'q' || price === -1)
             break;
         else if (price === '')
             continue;
-        paidAmount = readline.question('Enter Total of Received Amount: (or input \'Q\' to quit.) €');
-        if (paidAmount !== -1 && paidAmount !== 'Q' && paidAmount !== 'q')
-            console.log(getChange(price, paidAmount));
-        else if (paidAmount === '')
+        paidAmount = readline.question(' Enter Total of Received Amount: (or input \'Q\' to quit.) €');
+        if (paidAmount === '')
             continue;
+        if (paidAmount !== -1 && paidAmount !== 'Q' && paidAmount !== 'q')
+            console.log('\x1b[40m' + getChange(price, paidAmount) + '\x1b[0m');
     }
+    console.log('\n ')
 }
+main();
 // Examples:
 console.log(getChange(3.75, 50));
 // console.log(getChange(4.50, 20));
@@ -125,5 +129,4 @@ console.log(getChange(3.75, 50));
 
 //console.log(getChange('11131.11 EUR', '12000 EUR'));
 
-main();
 // console.log(getChange('23,43', '23,99'));
