c5300dda6240ac82479efabe49550fc132552fc5
Rami <103028944+Rami24t@users.noreply.github.com>
2022-09-07 14:27:11 +0200

    Update cashierProblem.js

--- cashierProblem.js
+++ cashierProblem.js
@@ -1,9 +1,8 @@
-'use strict';
-
-// Rami Al-Saadi                        The Cashier Problem - Project                               Sep 6 2022
+// Rami Al-Saadi                        The Cashier Problem - DCI Project                               Sep 6 2022
 
 // Problem:
-// Create a program that helps a cashier give adequate change to customers. The program should return the amount of notes and coins for the customer's change.
+// Create a program that helps a cashier give adequate change to customers.
+// The program should return the amount of notes and coins for the customer's change.
 // Example: If the price is €3.75 and the paid amount is €50, then the client should receive €46.25 back in change.
 //     The expected output should be:
 //     2 x €20
@@ -16,46 +15,44 @@
 //     1 x €10
 //     1 x €5
 //     1 x €0.5
-//     Notes
-//     Include outputs for exceptions e.g. price: €4, paid amount: €3.
+//     Note: Include outputs for exceptions
 
 
 // My implemented JS Solution:
-
-const getChange = (price = Infinity, paidAmount = 0) => {
-    if (+price !== +price) {
-        price = price.trim();
-        if (+price[0] === +price[0])
-            price = parseFloat(price)
-        else if (+price[1] === +price[1])
-            price = parseFloat(price.slice(1));
+'use strict';
+const readline = require("readline-sync");
+function processAmountInput(input) {
+    if (typeof input == typeof '1')
+        input = input.replaceAll(',', '.')
+    if (+input !== +input) {
+        input = input.trim();
+        if (+input[0] === +input[0])
+            input = parseFloat(input);
+        else if (+input[1] === +input[1])
+            input = parseFloat(input.slice(1));
         else
-            for (let i in price) {
-                if (+price[i] === +price[i] && price[i] !== ' ') {
-                    price = parseFloat(price.slice(i));
+            for (let i in input) {
+                if (+input[i] === +input[i] && input[i] !== ' ') {
+                    input = parseFloat(input.slice(i));
                     break;
                 }
             }
     }
-    if (+paidAmount !== +paidAmount) {
-        paidAmount = paidAmount.trim();
-        if (+paidAmount[0] === +paidAmount[0])
-            paidAmount = parseFloat(paidAmount)
-        else if (+paidAmount[1] === +paidAmount[1])
-            paidAmount = parseFloat(paidAmount.slice(1));
-        else
-            for (let i in paidAmount) {
-                if (+paidAmount[i] === +paidAmount[i] && paidAmount[i] !== ' ') {
-                    paidAmount = parseFloat(paidAmount.slice(i));
-                    break;
-                }
-            }
+    if (+input === +input) {
+        return +input;
     }
-    price = +price; paidAmount = +paidAmount;
-    let output = '\n';
-    let amount = (paidAmount - price) * 100;
+    else
+        return -1;
+}
+function getChange(price = Infinity, paidAmount = 0) {
+    price = processAmountInput(price);
+    paidAmount = processAmountInput(paidAmount);
+    if (price === -1 || paidAmount === -1)
+        return 'Invalid input!';
+    let output = `\n                             ${(new Date()).toString().slice(4, 21)}\n`;
+    let amount = (paidAmount * 100 - price * 100);
     if (price > paidAmount)
-        return '\nPrice is greater than the paid amount!\n (Price: ' + price.toFixed(2) + ' EUR, Paid amount: only ' + paidAmount.toFixed(2) + ' EUR.)\nPlease increase the paid amount by at least: ' + (price - paidAmount).toFixed(2) + ' EUR.\n';
+        return `\nPrice is greater than the paid amount!\n (Price: ${price.toFixed(2)} EUR, Paid amount: only ${paidAmount.toFixed(2)} EUR.)\nPlease increase the paid amount by at least: ${(price - paidAmount).toFixed(2)} EUR.\n`;
     else
         output += 'Price: €' + price.toFixed(2) + '\nReceived: €' + paidAmount.toFixed(2) + '\nChange: €' + (amount / 100).toFixed(2) + '\n';
     if (price !== paidAmount)
@@ -64,27 +61,37 @@ const getChange = (price = Infinity, paidAmount = 0) => {
         50000, 20000, 10000, 5000, 2000, 1000, 500,
         200, 100,
         50, 20, 10, 5, 2, 1
-    ]
-    const notesAndCoins = changesEuro.map(change => {
-        let amountChange = Math.floor(amount / change);
-        amount -= amountChange * change;
-        return amountChange
-    })
-    for (const idx in notesAndCoins) {
-        const number = notesAndCoins[idx]
+    ];
+    changesEuro.forEach((change, idx) => {
+        let number = Math.floor(amount / change);
+        amount -= number * change;
         if (number > 0)
             output += ' - ' + number + ' x €' + (idx > 8 ? (changesEuro[idx] / 100).toFixed(2) : changesEuro[idx] / 100) + (idx > 6 ? ' coin' : ' note') + (number > 1 ? 's' : '') + '\n';
-    }
+    });
     return output;
 }
-
+function main() {
+    let paidAmount = 0;
+    let price;
+    while (paidAmount !== -1 && paidAmount !== 'Q' && paidAmount !== 'q') {
+        price = readline.question('Enter Total Price: (or \'Q\' to quit) €');
+        if (price === 'Q' || price === 'q' || price === -1)
+            break;
+        else if (price === '')
+            continue;
+        paidAmount = readline.question('Enter Total of Received Amount: (or input \'Q\' to quit.) €');
+        if (paidAmount !== -1 && paidAmount !== 'Q' && paidAmount !== 'q')
+            console.log(getChange(price, paidAmount));
+        else if (paidAmount === '')
+            continue;
+    }
+}
 // Examples:
 console.log(getChange(3.75, 50));
 // console.log(getChange(4.50, 20));
 // console.log(getChange(4, 3));
 
 // console.log(getChange(23, 23));
-
 
 
 
@@ -117,3 +124,6 @@ console.log(getChange(3.75, 50));
 
 
 //console.log(getChange('11131.11 EUR', '12000 EUR'));
+
+main();
+// console.log(getChange('23,43', '23,99'));
